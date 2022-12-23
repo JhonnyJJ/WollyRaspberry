890004562d36790712c0d727b6cf77d79a9e3fc8
Matteo Giovanale <matteo.giovanale@di.unito.it>
2022-12-23 16:59:49 +0100

    changes

--- TEST/test vocale def.py
+++ TEST/test vocale def.py
@@ -1,7 +1,18 @@
 import os
 import speech_recognition as sr
+from gtts import gTTS
+from pygame import mixer
 
 run = True
+
+def textSpeech():
+    mixer.init()
+    print('parla')
+    text = 'Ciao sono Wolly!'
+    tts = gTTS(text=text)
+    tts.save('mp3/tts.mp3')
+    mixer.music.load('tts.mp3')
+    mixer.music.play()
     
 while run:
     with sr.Microphone() as source:
