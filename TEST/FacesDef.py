c63f75ef35731a795941049dab227960db375ec5
JhonnyJJ <m.giovanale14@gmail.com>
2023-03-30 22:02:13 +0200

    FaceDef created and working slow

---
+++ TEST/FacesDef.py
@@ -0,0 +1,56 @@
+import os
+import time
+from tkinter import *
+from PIL import Image, ImageTk
+
+espressione = "crazy"
+x = 1
+
+#initialize a Tk structure
+root = Tk()
+root.attributes('-fullscreen', True)
+
+# this are the dimension of the screen connected
+width = root.winfo_screenwidth()
+height = root.winfo_screenheight()
+
+#we create the label and then configure it with the image
+label = Label(root)
+label.pack()
+
+resized_image = Image.open("/home/wolly/Desktop/WollyRaspberry/img/faces/" + espressione + "/1.png").resize((width, height))
+imag = ImageTk.PhotoImage(resized_image)
+    
+def loop():
+    global espressione, x, imag
+    DIR = "/home/wolly/Desktop/WollyRaspberry/img/faces/" + espressione + "/"
+    face = [name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name)) and ".png" in name]
+    lenght = len(face)
+    if x <= lenght:
+        resized_image = Image.open(DIR + str(x) + ".png").resize((width, height))
+        imag = ImageTk.PhotoImage(resized_image)
+        label.configure(image=imag)
+        x += 1
+        root.after(1, loop)
+    else:
+        x = 1
+        espressione = "default" 
+    
+    
+def niamPool():
+    loop()
+    root.after(20000, niamPool)
+        
+
+def change_face(esprex):
+    global espressione
+    espressione = esprex
+    
+    
+def main():
+    root.after(0,niamPool)
+    root.mainloop()
+    
+if __name__ =="__main__":
+    main()
+    
