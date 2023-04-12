1ff8817a201a90e2570063c8c4361763a0492c4e
JhonnyJJ <m.giovanale14@gmail.com>
2023-04-12 14:37:01 +0200

    aa

--- Wolly MG/WollyTotal.py
+++ Wolly MG/WollyTotal.py
@@ -17,7 +17,7 @@ import re
 # import trackingFace
 import cv2
 import random
-from motorsNew import avanti, indietro, destra, sinistra
+
 
 # import frasi necessarie
 from frasi import *
@@ -29,7 +29,7 @@ def signal_handler(sig, frame):
     process1.terminate()
     process2.terminate()
     process3.terminate()
-    process4.terminate()
+    # process4.terminate()
     time.sleep(2)
     sys.exit(0)
 
@@ -196,7 +196,7 @@ def chatInit():
                     stopTracking()
                     print(random.choice(responses) + random.choice(posso))
                     textSpeech(random.choice(responses) + random.choice(posso))
-
+                    reface("wink")
                     response = talk()
                     if response is not False:
                         chat(response)
@@ -234,14 +234,10 @@ def default():
     im = ImageTk.PhotoImage(Image.open(direct))
     label.configure(image=im)
     t.mainloop()
-
-
 # ----------fine processo 3--------------
 
-# ------------------PROCESSO 4------------------
-# processo 4 che fa andare il tracking della faccia che può essere stoppato quando non serve e attivato quando torna in silenzio
-
-def track():
+# ---------- cam --------------
+def cam():
     numFaces = 0
     # cascade classifier for face tracking
     face_cascade = cv2.CascadeClassifier('/home/wolly/Desktop/WollyRaspberry/lib/haarcascade_frontalface_default.xml')
@@ -272,94 +268,24 @@ def track():
 
         # face detection for each frame
         faces = face_cascade.detectMultiScale(gray, 1.1, 3, 0, (10, 10))
-
-        # if the number of faces is greater than the number of faces already recognised Wolly will introduce himself again
-        if 0 < len(faces) != numFaces:
-            if len(faces) > numFaces:
-                print("TTS", len(faces), numFaces)
-                textSpeech(random.choice(ciao) + random.choice(ascolto) + random.choice(quando))
-                reface("wink")
-                numFaces = len(faces)
-            print(len(faces), numFaces)
-
-        print("Found " + str(len(faces)) + " face(s)")
-
-        # create a green rectangle around the found faces
-        for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-            # start tracking
-            # values of the frame to find the center on the x axis of the frame
-            FRAME_W = 320
-
-            # centre of the face (only x axis because it hasn't any motor on the y axis)
-            x = x + (w / 2)
-
-            # coordinates on the right side, of the robot while facing you, is greater then left side (any future right or left will be referred to the robot)
-            # we take the half of the frame wight and add 80 frames of "dead zone" (40 on each side) so the robot isn't constantly moving
-            deadzone_rx = (FRAME_W / 2) + 40
-            deadzone_sx = (FRAME_W / 2) - 40
-
-            # we create a border to make bigger adjustments so the robot can keep up with the face
-            correct_rx = (FRAME_W / 2) + 70
-            correct_sx = (FRAME_W / 2) - 70
-
-            # if the face coordinate is < of the left border the robot needs to be moving left
-            # if the face coordinate is > of the right border the robot needs to be moving right
-            if x < deadzone_sx and x > correct_sx:
-                sinistra(0.05, 0.8)
-
-            elif x > deadzone_rx and x < correct_rx:
-                destra(0.05, 0.8)
-
-            elif x > correct_rx:
-                destra(0.1, 0.8)
-
-            elif x < correct_sx:
-                sinistra(0.1, 0.8)
-
-            # if the width of the rectangle is greater than 110 it means that the face is too close to the robot, vice versa if it's lower than 52
-            if w > 120:
-                indietro(0.09, 0.8)
-
-            elif w < 60:
-                avanti(0.09, 0.8)
-
-            break
-
-        # resize and flip to show camera feed with squares
-        # creation of the camera feed window
-
-        # frame = cv2.resize(frame, (320,200))
-        # frame = cv2.flip(frame,1)
-        # cv2.imshow('Video', frame)
-
-    cap.release()
-    # cv2.destroyAllWindows()
-
-
-def startTracking():
+        
+def startCam():
     global process4
-    process4 = multiprocessing.Process(target=track)
+    process4 = multiprocessing.Process(target=cam)
     process4.start()
 
-
-def stopTracking():
+def stopCam():
     global process4
     process4.terminate()
-
-
-# ----------fine processo 4--------------
+    
 
 if __name__ == '__main__':
     print('Press Ctrl+C to stop the program')
     process1 = multiprocessing.Process(target=face)
     process2 = multiprocessing.Process(target=chatInit)
     process3 = multiprocessing.Process(target=default)
-    process4 = multiprocessing.Process(target=track)
     process3.start()
     process1.start()
     process2.start()
     time.sleep(1)
-    process4.start()
     signal.signal(signal.SIGINT, signal_handler)
