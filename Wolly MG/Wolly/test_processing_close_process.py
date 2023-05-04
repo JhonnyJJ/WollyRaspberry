fdff9abf1917638e53bb1c84768a7327c6a6f16f
JhonnyJJ <m.giovanale14@gmail.com>
2023-05-04 12:47:42 +0200

    Wolly is working as expected

--- Wolly MG/Wolly/test_processing_close_process.py
+++ Wolly MG/Wolly/test_processing_close_process.py
@@ -1,6 +1,8 @@
 import multiprocessing
 import time
-import psutil
+from os import kill
+from os import getpid
+from signal import SIGKILL
 
 def chiudi2():
     global process2, pids
@@ -17,34 +19,36 @@ def start2():
     
     
     
-def chatInit():
-    global process2, parentpid
+def chatInit(event, i):
+    global process2
     print("chiusura faccia")
     chiudi2()
-    parentpid = multiprocessing.current_process().pid
-    while True:
+    while not event.is_set():
         print("chat {}".format(multiprocessing.current_process().pid))
         time.sleep(2)
+    process2.terminate()
         
     
 def faccia():
-    pids = multiprocessing.current_process().pid
+    global pids
+    pids = getpid()
     while True:
         print("ciao")
         time.sleep(2)
         print("faccia {}".format(multiprocessing.current_process().pid))
     
 def master():
-    global process2, parentpid
+    global process2, pids
+    event = multiprocessing.Event()
     process2 = multiprocessing.Process(target=faccia)
     process2.start()
     time.sleep(3)
-    process3 = multiprocessing.Process(target=chatInit)
+    process3 = multiprocessing.Process(target=chatInit, args=(event, i))
     process3.start()
     print("aspetto chiusura p2")
-    time.sleep(12)
-    parent = psutil.Process(parentpid)
-    parent.terminate()
+    time.sleep(10)
+    event.set()
+    process3.join()
     print("CHIUDO DEF FACCIA")
     
     
